f1ea0cc5402fed90a1059fb9c4651e5baf85c01f
Happyday <happyday.mjohnson@gmail.com>
2024-03-19 05:05:33 -0700

    evolving documentation.

--- workflow_error_code.py
+++ workflow_error_code.py
@@ -1,3 +1,37 @@
+###########################################################################################
+# Author: HappyDay Johnson
+# Version: 0.01
+# Date: 2024-03-20
+# Summary: workflow_error_code centralizes error handling in the transcription
+# workflow using an `async_error_handler` decorator. The decorator wraps asynchronous functions,
+# capturing exceptions and logging detailed error information including stack traces. It
+# simplifies error management by automatically handling try/except blocks, enriching error messages
+# with operation-specific details, and optionally re-raising exceptions for further handling.
+# Additionally, it defines custom exceptions for granular control over workflow-related errors,
+# facilitating clearer debugging and error resolution strategies.
+#
+# License Information: MIT License
+#
+# Copyright (c) 2024 HappyDay Johnson
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+###########################################################################################
 
 import traceback
 from functools import wraps
@@ -6,6 +40,21 @@ from logger_code import LoggerBase
 
 
 async def handle_error(error_message: str=None, operation=None, raise_exception=True):
+    """
+    Logs an error message with optional operation context and raises a generic exception if specified.
+
+    This function assembles an error message from provided details and logs it using the system's
+    logger. If `raise_exception` is True, it raises a generic Exception with the constructed error
+    message, allowing for standardized error handling across asynchronous functions.
+
+    Args:
+        error_message (str, optional): The error message to log. Defaults to None, which results in 'Error: Unknown'.
+        operation (str, optional): The operation during which the error occurred. Defaults to None, indicating an unknown operation.
+        raise_exception (bool, optional): Whether to raise an exception after logging the error. Defaults to True.
+
+    Raises:
+        Exception: A generic exception with the error message, if `raise_exception` is True.
+    """
     logger = LoggerBase.setup_logger('handle_error')
     # Dynamically construct parts of the message based on non-None values
     parts = [
@@ -20,6 +69,24 @@ async def handle_error(error_message: str=None, operation=None, raise_exception=
         raise Exception(exception_message) # pylint: disable=broad-exception-raised
 
 def async_error_handler(error_message=None, raise_exception=True):
+    """
+    Decorator that wraps asynchronous functions for standardized error handling and logging.
+
+    This decorator catches exceptions thrown by the decorated asynchronous function, logs a detailed error
+    message including a stack trace, and optionally re-raises the exception. It allows for enriching the
+    error message with a custom message or uses the exception's message if none is provided. The detailed
+    error message is then logged through a centralized error handling function (`handle_error`), which also
+    takes care of logging the operation name and deciding whether to raise a generic exception based on the
+    decorator's parameters.
+
+    Args:
+        error_message (str, optional): Custom error message to use instead of the exception's message. Defaults to None.
+        raise_exception (bool, optional): Whether to re-raise the caught exception after logging. Defaults to True.
+
+    Returns:
+        function: A wrapper function that incorporates error handling into the decorated asynchronous function.
+
+    """
     def decorator(func):
         @wraps(func)
         async def wrapper(*args, **kwargs):
